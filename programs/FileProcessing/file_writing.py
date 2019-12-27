c187c7e7d7225590de7798b2b74b1c5c49318708
tejas.dongre@oracle.com <tejas.dongre@oracle.com>
2019-12-27 16:41:42 +0530

    Added the modes of accessing a file
    Added the modes of accessing a file

--- programs/FileProcessing/file_writing.py
+++ programs/FileProcessing/file_writing.py
@@ -10,3 +10,25 @@ with open("bear.txt",mode="r") as bear_file:
     file_txt = open("first.txt",mode="w") 
     file_txt.write(file_content)
     file_txt.close()
+
+
+# with open("bear.txt",mode="r") as bear_file:   
+#     file_content = bear_file.read()[:90]
+#     file_txt = open("first.txt",mode="x") # x mode will create the file if doesn't exust and writes into it
+#     file_txt.write(file_content)
+#     file_txt.close()
+
+with open("bear.txt",mode="r") as bear_file:   
+    file_content = bear_file.read()[:90]
+    file_txt = open("first.txt",mode="a") # a mode will append the data to the existing file
+    file_txt.write(file_content)
+    file_txt.close()
+
+with open("bear.txt",mode="r") as bear_file:   
+    file_content = bear_file.read()[:90]
+    file_txt = open("first.txt",mode="a+") # a+ mode will allow to read and write data simultaneously
+    file_txt.write(file_content)
+    file_txt.seek(0) # this will set the cursor to the original position
+    content = file_txt.read()
+    print(content)
+    file_txt.close()
