7a4c6e41770e1008aa61bbbfcbdd6d44e13f82ae
tejas.dongre@oracle.com <tejas.dongre@oracle.com>
2019-12-28 10:39:58 +0530

    Added a panda module
    Usage of panda module to read the csv file and get the mean of each of the columns in the module

--- programs/ModulesRelated/importing_modules_usage.py
+++ programs/ModulesRelated/importing_modules_usage.py
@@ -1,10 +1,17 @@
-import os
+
+
+#use this after installing pandas library 
+# This takes a csv file as the input which has two columns and gives the mean of each of the column.
+# It also prints the mean of one column st1 which can be accessed as seen in the code.
+import pandas
 import time
+import os
+
 while True:
-    if os.path.exists("/Users/tejasdongre/Tejas/Personal/Python/10_real_world_app_course/python_apps/python-apps-master/programs/FileProcessing/data.txt"):
-        with open("/Users/tejasdongre/Tejas/Personal/Python/10_real_world_app_course/python_apps/python-apps-master/programs/FileProcessing/data.txt") as file :
-            print(file.read())
+    if os.path.exists("/Users/tejasdongre/Tejas/Personal/Python/10_real_world_app_course/python_apps/python-apps-master/programs/ModulesRelated/temps-today.csv"):
+        content = pandas.read_csv("/Users/tejasdongre/Tejas/Personal/Python/10_real_world_app_course/python_apps/python-apps-master/programs/ModulesRelated/temps-today.csv")
+        print(content.mean())
+        print(content.mean()["st1"])
     else:
-        print("File does not exist")
-        print(__file__)
-    time.sleep(5)
+        print("file does not exist")
+    time.sleep(10)
--- the_basics/python_help_ex.py
+++ the_basics/python_help_ex.py
@@ -2,3 +2,4 @@
 # dir(list) gives all the functionaities that can be performed by the lst datatype
 # help(list.sort)  this gives the details of the attribute/function  present in the list datatype
 # dir(__builtins__) this gives the list of all builtin functions/datatypes - for example - print() and list
+# pip3 install pandas will install a third party library called pandas ( pip is used if python 2 is installed and pip3 is installed if python3 is installed)
